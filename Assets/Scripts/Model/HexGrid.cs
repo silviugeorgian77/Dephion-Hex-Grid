1c9b16d139ebb23dc1c401b02f8e76a11b8e3ced
Silviu <silviu.georgian77@icloud.com>
2022-11-13 10:54:52 +0200

    Added models for HexGrid and HexTile

---
+++ Assets/Scripts/Model/HexGrid.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+[Serializable]
+public class HexGrid
+{
+    [JsonProperty("TileSize")]
+    public float tileSize;
+
+    [JsonProperty("TilePadding")]
+    public float tilePadding;
+
+    [JsonProperty("DefaultTileColor")]
+    public string defaultTileColor;
+
+    [JsonProperty("Tiles")]
+    public List<HexTile> tiles;
+}
---
+++ Assets/Scripts/Model/HexTile.cs
@@ -0,0 +1,12 @@
+using System;
+using Newtonsoft.Json;
+
+[Serializable]
+public class HexTile
+{
+    [JsonProperty("Index")]
+    public int index;
+
+    [JsonProperty("ClickedColor")]
+    public string clickedColor;
+}
